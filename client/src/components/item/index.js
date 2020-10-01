a7637a2c07af69459c04dd76b39d5a20d442a6a5
tuguldur <aa402115@gmail.com>
2020-10-01 17:10:35 +0800

    Bug killed

--- client/src/components/item/index.js
+++ client/src/components/item/index.js
@@ -6,17 +6,23 @@ const Item = (item) => {
     <div className="media-card">
       <Link
         className="cover"
-        to={`/titles/${item.titles[item.titles.length - 1].id}`}
+        to={`/titles/${
+          item.home ? item.titles[item.titles.length - 1].id : item.titles[0].id
+        }`}
       >
         <Image
           src={`https://api.anikodcdn.net/${
-            item.titles[item.titles.length - 1].images.poster.md
+            item.home
+              ? item.titles[item.titles.length - 1].images.poster.md
+              : item.titles[0].images.poster.md
           }`}
           alt={item.name}
         />
       </Link>
       <Link
-        to={`/titles/${item.titles[item.titles.length - 1].id}`}
+        to={`/titles/${
+          item.home ? item.titles[item.titles.length - 1].id : item.titles[0].id
+        }`}
         className="name"
       >
         {item.name}
