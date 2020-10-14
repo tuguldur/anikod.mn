d1fc230d387a69ceff8ac929765755add097be7a
tuguldur <aa402115@gmail.com>
2020-10-14 12:53:20 +0800

    Opening/Ending skip

--- client/src/components/player/index.js
+++ client/src/components/player/index.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./style.scss";
-const Player = ({ src, sources }) => {
+const Player = ({ src, timestamps, skip }) => {
   const video = useRef(null);
   const [media, setMedia] = useState(null);
-  useEffect(() => { 
-  
-  }, [media]);
+  useEffect(() => {
+    const skip_enabled = skip
+      ? skip === "true" || skip === true
+        ? true
+        : false
+      : false;
+    if (video.current && skip_enabled) {
+      video.current.addEventListener("timeupdate", () => {
+        if (
+          timestamps.op_start &&
+          Number(timestamps.op_start) ===
+            Number(video.current.currentTime.toFixed())
+        ) {
+          console.log("Opening Jumped to: " + timestamps.op_end);
+          video.current.currentTime = Number(timestamps.op_end);
+        }
+        if (
+          timestamps.ed_start &&
+          Number(timestamps.ed_start) ===
+            Number(video.current.currentTime.toFixed())
+        ) {
+          console.log("Ending Jumped to: " + timestamps.ed_end);
+          video.current.currentTime = Number(timestamps.ed_end);
+        }
+      });
+    }
+    console.log("some thing running");
+  }, [media, skip, timestamps]);
   return (
     <div className="player">
       <video
--- client/src/pages/stream/index.js
+++ client/src/pages/stream/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { MDCMenuSurface } from "@material/menu-surface";
 import { Link } from "react-router-dom";
+import { MDCSwitch } from "@material/switch";
 import "./style.scss";
 // player
 import { Player } from "../../components";
@@ -10,14 +11,16 @@ const Stream = (props) => {
   const [episode, setEpisode] = useState(null);
   const [title, setTitle] = useState(null);
   const [episodes, setEpisodes] = useState(null);
-  const [resolution,setResolution] = useState(null);
+  const [resolution, setResolution] = useState(null);
+  const [skip, setSkip] = useState(localStorage.getItem("skip"));
+
   useEffect(() => {
     // https://api.anikodcdn.net/api/episodes/:id
     axios
       .get("https://api.anikodcdn.net/api/episodes/" + Number(id))
       .then((data) => {
         setEpisode(data.data);
-        setResolution(data.data.vods[0].resolution)
+        setResolution(data.data.vods[0].resolution);
         // https://api.anikodcdn.net/api/titles/:title_id
         axios
           .get("https://api.anikodcdn.net/api/titles/" + data.data.title_id)
@@ -40,6 +43,16 @@ const Stream = (props) => {
           });
       });
   }, [id]);
+  useEffect(() => {
+    if (title && episode && episodes) {
+      var switchEl = new MDCSwitch(document.getElementById("switch-ripple"));
+      switchEl.checked = skip
+        ? skip === "true" || skip === true
+          ? true
+          : false
+        : false;
+    }
+  });
   return (
     <div id="stream">
       <div className="modal" role="dialog">
@@ -47,41 +60,58 @@ const Stream = (props) => {
           <>
             <div className="modal-header">
               <h1>
-                <span>
-                  {title.name_mn}
-                </span>
+                <span>{title.name_mn}</span>
               </h1>
               <div className="action-wrapper">
-              <div className="mdc-menu-surface--anchor">
-                <button className="material-icons mdc-icon-button"
-                onClick={()=>{
-                  const menu = new MDCMenuSurface(document.querySelector("#player-settings"));
-                  menu.open();
-                }}
-                >more_vert</button>
-                <div className="mdc-menu-surface" id="player-settings">
-                  <div className="menu-title">Quality</div>
-            <div className="mdc-list">
-            {
-              episode.vods.map((res,index)=>(
-<div
-                 
-                 className={`mdc-list-item${resolution === res.resolution ? ' active' :''}`}
-                 key={index}
-              onClick={()=>setResolution(res.resolution)}
-               >
-                 <span className="mdc-list-item__ripple"></span>
-                 {res.resolution}p
-               </div>
-              ))
-            }
-           
-              <span className="mdc-list-item__ripple"></span>
-           
-            </div>
-              </div>
-              </div>
-
+                <div className="mdc-menu-surface--anchor">
+                  <button
+                    className="material-icons mdc-icon-button"
+                    onClick={() => {
+                      const menu = new MDCMenuSurface(
+                        document.querySelector("#player-settings")
+                      );
+                      menu.open();
+                    }}
+                  >
+                    more_vert
+                  </button>
+                  <div className="mdc-menu-surface" id="player-settings">
+                    <div className="menu-title">Quality</div>
+                    <div className="mdc-list">
+                      {episode.vods.map((res, index) => (
+                        <div
+                          className={`mdc-list-item${
+                            resolution === res.resolution ? " active" : ""
+                          }`}
+                          key={index}
+                          onClick={() => setResolution(res.resolution)}
+                        >
+                          <span className="mdc-list-item__ripple"></span>
+                          {res.resolution}p
+                        </div>
+                      ))}
+                      <span className="mdc-list-item__ripple"></span>
+                    </div>
+                    <div className="menu-title">Opening/Ending алгасах</div>
+                    <div className="settings-item">
+                      <div className="mdc-switch" id="switch-ripple">
+                        <div className="mdc-switch__track"></div>
+                        <div className="mdc-switch__thumb-underlay">
+                          <div className="mdc-switch__thumb"></div>
+                          <input
+                            type="checkbox"
+                            className="mdc-switch__native-control"
+                            role="switch"
+                            onChange={(e) => {
+                              localStorage.setItem("skip", e.target.checked);
+                              setSkip(e.target.checked);
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <Link
                   to={"/titles/" + episode.title_id}
                   className="material-icons mdc-icon-button"
@@ -92,6 +122,8 @@ const Stream = (props) => {
             </div>
             <div className="modal-player-content">
               <Player
+                skip={skip}
+                timestamps={episode.timestamps}
                 src={`https://anikodcdn.net/static/media/mp4/${episode.title_id}/${episode.number}_${resolution}.mp4`}
               />
             </div>
